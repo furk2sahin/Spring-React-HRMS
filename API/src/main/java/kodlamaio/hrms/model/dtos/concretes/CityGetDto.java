8050f5ddb2ee88ca4dfdf491a75d398ad0f9faa0
furk2sahin <furkannsahin.7@gmail.com>
2021-06-01 22:57:20 +0300

    Add Dtos for City

---
+++ API/src/main/java/kodlamaio/hrms/model/dtos/concretes/CityGetDto.java
@@ -0,0 +1,12 @@
+package kodlamaio.hrms.model.dtos.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityGetDto {
+    private String name;
+}
